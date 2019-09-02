082fedf07357122d9a3d97e2a3718501caf45277
Ofir <otalmor@lightricks.com>
2019-09-02 17:22:43 +0300

    PlayingCard implemented

--- MatchCards/PlayingCard.h
+++ MatchCards/PlayingCard.h
@@ -16,6 +16,9 @@ NS_ASSUME_NONNULL_BEGIN
 @property (strong, nonatomic) NSString *suit;
 @property (nonatomic) NSUInteger rank;
 
++ (NSArray *) validSuits;
++ (NSUInteger) maxRank;
+
 @end
 
 NS_ASSUME_NONNULL_END
