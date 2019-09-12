8899a9b66008b9e737e999b0cdbc50f1a05ea1c3
Ofir <otalmor@lightricks.com>
2019-09-12 14:17:11 +0300

    Deck changed to a protocol

--- MatchCards/PlayingCardDeck.h
+++ MatchCards/PlayingCardDeck.h
@@ -10,7 +10,7 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface PlayingCardDeck : Deck
+@interface PlayingCardDeck : NSObject <Deck>
 
 @end
 
--- MatchCards/SetDeck.h
+++ MatchCards/SetDeck.h
@@ -11,7 +11,7 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface SetDeck : Deck
+@interface SetDeck : NSObject <Deck>
 
 @end
 
